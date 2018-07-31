4e1595e94357561fd750f459295eb2807c6cb327
kazu <kazu20120801@gmail.com>
2018-07-31 19:20:39 +0900

    詳細ページの実装

--- App.js
+++ App.js
@@ -12,9 +12,11 @@ export default StackNavigator ({
   },
   Detail: {
     screen: Detail,
-    navigationOptions: {
-      title: 'Detail',
-    }
+
+    // ↓動的に変更するひとつのやりかた(別の方法はDetail.js,表示させたいページにて)
+    navigationOptions: ({ navigation }) => ({ // objectだとtitleなどは固定のものしか入らない。関数にすると動的に変更可能
+      title: navigation.state.params.item.name,
+    })
   }
 },{
   initialRouteName: 'Home' // 初期表示させる画面
--- Detail.js
+++ Detail.js
@@ -1,4 +1,51 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
-export default ({ navigation }) => console.log(navigation) || <Text>{navigation.state.params.item.name}</Text>;
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  repoName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  ownerIcon: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    marginRight: 5,
+  },
+  ownerName: {
+    fontSize: 14
+  },
+  repoUrl: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
+
+export default class Detail extends React.Component {
+
+  // ページ毎のnaviオプション
+  // static navigationOptions = ({ navigation }) => ({
+  //   title: navigation.state.params.item.name,
+  // })
+
+  render() {
+    const { navigation: { state: { params: { item }}} } = this.props;
+    console.log(item);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.fullName}>{item.full_name}</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Image style={styles.ownerIcon} source={{ url: item.owner.avatar_url }}/>
+            <Text style={styles.ownerName}>{item.owner.login}</Text>
+          </View>
+        <Text>{item.description}</Text>
+        <Text style={styles.repoUrl}>{item.url}</Text>
+      </View>
+    );
+  }
+}
